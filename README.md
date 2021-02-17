1dcafe49b448872cff3aed32d52cd2c559ec7188
jiangshan00001 <710806594@qq.com>
2021-02-17 11:16:31 +0800

    add  wget

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # qt_static
 qt static build test
+
+https://doc.qt.io/qt-5/windows-building.html
+https://github.com/fpoussin/Qt5-MSVC-Static
+https://retifrav.github.io/blog/2018/02/17/build-qt-statically/#qt-5101-windows-10-and-mingw-x32
+
+https://stackoverflow.com/questions/61737910/linking-qt-with-github-actions-using-mingw
--- qt_5.12.10.build.bat
+++ qt_5.12.10.build.bat
@@ -1,5 +1,7 @@
+cinst Wget;
 wget https://download.qt.io/archive/qt/5.12/5.12.10/single/qt-everywhere-src-5.12.10.tar.xz
 tar -xvJf qt-everywhere-src-5.12.10.tar.xz
 cd ./qt-everywhere-src-5.12.10;
 ./configure.bat -release -static -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "..\qt51210_static\" -skip webengine -nomake tools -nomake tests -nomake examples 
-make ;
+REM make ;
+mingw32-make
