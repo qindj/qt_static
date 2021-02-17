080f2482fad964251a015d8796d54ae49d77cd07
jiangshan00001 <710806594@qq.com>
2021-02-17 16:15:04 +0800

    Update qt_5.12.10.build.bat

--- qt_5.12.10.build.bat
+++ qt_5.12.10.build.bat
@@ -3,8 +3,9 @@ choco install archiver;
 
 wget https://download.qt.io/archive/qt/5.12/5.12.10/single/qt-everywhere-src-5.12.10.tar.xz
 arc unarchive qt-everywhere-src-5.12.10.tar.xz
-#tar -xvJf qt-everywhere-src-5.12.10.tar.xz
+REM #tar -xvJf qt-everywhere-src-5.12.10.tar.xz
+ls;
 cd ./qt-everywhere-src-5.12.10;
-./configure.bat -release -static -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "..\qt51210_static\" -skip webengine -nomake tools -nomake tests -nomake examples 
+configure.bat -release -static -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "..\qt51210_static\" -skip webengine -nomake tools -nomake tests -nomake examples 
 REM make ;
 mingw32-make
