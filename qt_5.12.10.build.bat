688e940595292c929222593fe3ba691362e4a9bd
jiangshan00001 <710806594@qq.com>
2021-02-18 15:29:01 +0800

    Update qt_5.12.10.build.bat

--- qt_5.12.10.build.bat
+++ qt_5.12.10.build.bat
@@ -6,6 +6,6 @@ arc unarchive qt-everywhere-src-5.12.10.tar.xz
 REM #tar -xvJf qt-everywhere-src-5.12.10.tar.xz
 ls
 cd qt-everywhere-src-5.12.10
-configure.bat -release -static -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "..\qt51210_static\" -skip webengine -nomake tools -nomake tests -nomake examples  -opensource
+configure.bat -release -static -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "..\qt51210_static\" -skip webengine -nomake tools -nomake tests -nomake examples  -opensource yes -commercial no
 REM make ;
 mingw32-make
