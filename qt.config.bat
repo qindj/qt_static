f1ea0d2f3b1f5296e0ee83151f844007ca898ca8
jiangshan00001 <710806594@qq.com>
2021-02-23 11:55:30 +0800

    Update qt.config.bat

--- qt.config.bat
+++ qt.config.bat
@@ -8,8 +8,10 @@ choco install python2
 
 wget https://download.qt.io/archive/qt/%QT_VER_FRONT%/%QT_VER_ALL%/single/qt-everywhere-opensource-src-%QT_VER_ALL%.tar.xz
 wget https://download.qt.io/archive/qt/%QT_VER_FRONT%/%QT_VER_ALL%/single/qt-everywhere-src-%QT_VER_ALL%.tar.xz
+mv qt-everywhere-opensource-src-%QT_VER_ALL%.tar.xz qt-everywhere-src-%QT_VER_ALL%.tar.xz
 arc unarchive qt-everywhere-src-%QT_VER_ALL%.tar.xz
-arc unarchive qt-everywhere-opensource-src-%QT_VER_ALL%.tar.xz
+
+mv qt-everywhere-opensource-src-%QT_VER_ALL% qt-everywhere-src-%QT_VER_ALL%
 
 REM #tar -xvJf qt-everywhere-src-%QT_VER_ALL%.tar.xz
 ls
