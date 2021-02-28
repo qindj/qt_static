57d94087d98f3e468692903d2dbec0d7b83dedba
jiangshan00001 <710806594@qq.com>
2021-02-28 11:25:21 +0800

    config test2

--- qt.config.bat
+++ qt.config.bat
@@ -4,4 +4,4 @@ set QT_VER_ALL=%2
 
 ls
 cd qt-everywhere-src-%QT_VER_ALL%
-configure.bat  -release -static  -opensource -confirm-license   -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "D:\\a\\qt_static\\qt_static\\qt-everywhere-src-%QT_VER_ALL%\\..\\qt_static_lib\\" -skip webengine -nomake tools -nomake tests -nomake examples 
+configure.bat  -release -static  -opensource -confirm-license -mp   -ltcg -no-pch -optimize-size -opengl desktop -platform win32-g++ -prefix "D:\\a\\qt_static\\qt_static\\qt-everywhere-src-%QT_VER_ALL%\\..\\qt_static_lib\\" -skip webengine -skip qtwebkit -nomake tools -nomake tests -nomake examples 
--- qt_5.12.10.make.bat
+++
@@ -1,5 +0,0 @@
-
-cd D:\\a\\qt_static\\qt_static\\qt-everywhere-src-5.12.10\\
-cd D:
-ls
-mingw32-make
